20ebe8e84a682080968a55ff698c95a0eadd2493
Maxwell <MaxwellFX@users.noreply.github.com>
2018-08-16 15:05:50 -0700

    Update DataVisualization.py

--- DataVisualization.py
+++ DataVisualization.py
@@ -5,11 +5,11 @@ import numpy as np
 def plot_steering_historgram(viz_settings):
     num_bins = viz_settings["num_bins"]
     steering_angles = viz_settings["steering_angles"]
-    avg_samples = viz_settings["avg_samples_per_bin"]
+    count_cutoff = viz_settings["count_cutoff"]
 
     hist, bins = np.histogram(steering_angles, num_bins)
     width = 0.7 * (bins[1] - bins[0])
     center = (bins[:-1] + bins[1:]) / 2
     plt.bar(center, hist, align='center', width=width)
-    plt.plot((np.min(steering_angles), np.max(steering_angles)), (avg_samples, avg_samples), 'k-')
+    plt.plot((np.min(steering_angles), np.max(steering_angles)), (count_cutoff, count_cutoff), 'k-')
     plt.show()
